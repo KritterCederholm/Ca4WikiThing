70d8933699d4a14f7097b4cd8a283aa987828710
TanjaCe <minpaloma@hotmail.com>
2014-11-19 11:23:10 +0100

    Status commit

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -4,10 +4,12 @@ angular.module('myAppRename.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: '/partials/partial1',
+    templateUrl: 'app/view1/view1.html', //templateUrl: '/partials/partial1',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', function() {
-});
+.controller('View1Ctrl', ['$scope','InfoFactory','InfoService', function($scope,InfoFactory,InfoService) {
+        $scope.infoFactory = InfoFactory.getInfo();
+        $scope.infoService = InfoService.getInfo();
+}]);
--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -2,24 +2,87 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var user = mongoose.model('wiki');
+var wiki = mongoose.model('wiki');
 
-/* GET A wiki From The DataBase */
+/*/Hvad er grunden til at dette ikke fungere? Hvordan kan det komme til at virke! Svar: skriv '/api' før 'Hello'.
+router.get('/Hello', function(req, res) {
+   res.send("Hello there...");
+});
+*/
+
 router.get('/wiki', function(req, res) {
+    if(typeof global.mongo_error !== "undefined"){
+        res.status(500);
+        res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+        return;
+    }
+    wiki.find({}, function (err, wiki) {
+        if (err) {
+            res.status(err.status || 400);
+            res.end(JSON.stringify({error: err.toString()}));
+            return;
+        }
+        res.header("Content-type","application/json");
+        res.end(JSON.stringify(wiki));
+    });
+});
+
+/*
+function getWiki(title) {
+    if(typeof global.mongo_error !== "undefined"){
+        return "Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)";
+    }
+    wiki.findByTitle(title,function (obj) {
+        return obj;
+    })
+
+    return "some wikipage";
+}
+/* GET A wiki From The DataBase //
+
+router.get('/getWiki/:title', function(req, res) {
   if(typeof global.mongo_error !== "undefined"){
     res.status(500);
     res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
     return;
+
   }
-  wiki.find({}, function (err, users) {
+    var requestedTitle =  req.params.title;
+  wiki.find({ title : requestedTitle}, function (err, wikiSchema) {
     if (err) {
       res.status(err.status || 400);
       res.end(JSON.stringify({error: err.toString()}));
       return;
     }
+      console.log(requestedTitle);
     res.header("Content-type","application/json");
-    res.end(JSON.stringify(users));
+    res.end(JSON.stringify(wikiSchema));
   });
 });
 
+router.get('/findWiki/:searchString', function(req, res) {
+    if(typeof global.mongo_error !== "undefined"){
+        res.status(500);
+        res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+        return;
+
+    }
+    var requestedSearchString = req.params.searchString;
+    wiki.find( $or [{ title : requestedSearchString}, {}], function (err, wikiSchema) {
+        if (err) {
+            res.status(err.status || 400);
+            res.end(JSON.stringify({error: err.toString()}));
+            return;
+        }
+        res.header("Content-type","application/json");
+        res.end(JSON.stringify(wikiSchema));
+    });
+});
+*/
+
 module.exports = router;
+
+//getWiki(title);
+//findWiki(searcgString);
+//getCategories();
+//getWikiesWithCategory(category);
